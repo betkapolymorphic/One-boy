0409c34cbc1525cd9790c54f501e2f5898e5777c
betkapolymorphic <a_albert@ukr.net>
2014-06-26 10:21:18 +0300

    upload db migrate

--- db/migrate/20140624114608_create_images.rb
+++ db/migrate/20140624114608_create_images.rb
@@ -6,8 +6,8 @@ class CreateImages < ActiveRecord::Migration
     t.integer :colortype2
     t.integer :colortype3
     t.integer :colortype4
-    t.string  :url
-    t.string :rgb_colors
+    t.text  :url
+    t.text :rgb_colors
     t.timestamps
     end
   end
